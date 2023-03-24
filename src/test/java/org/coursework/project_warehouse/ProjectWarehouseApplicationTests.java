6e17f86468b29d0723384e964bee5bfa3c27a75a
Roman <114007909+Clay89tms@users.noreply.github.com>
2023-03-24 12:55:00 +0300

    Coursework com1 [structure]

---
+++ src/test/java/org/coursework/project_warehouse/ProjectWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coursework.project_warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
